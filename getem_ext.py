39a7dbe5a63ec74888329983ed777b361516b86f
mboogn <61652919+mboogn@users.noreply.github.com>
2022-09-13 10:23:01 -0600

    Add files via upload

---
+++ getem_ext.py
@@ -0,0 +1,11 @@
+
+import sys
+sys.path.append("/storage/emulated/0/Documents/pythonstuff/Algevaric/_funq_/functuality/")
+from functual_higher_order import *
+from functual_higher_order import __all__, __doc__
+
+
+__name__ = "getem_ext"
+
+
+
